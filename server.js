be6bae3963c2c9186df8f6b6c9267978900c8a17
Patrick Kleckner <p.kleckner@killerpom.com>
2017-01-04 19:31:15 -0500

    Remove duplicate variable
    Variable 'path' was created twice. Removed one.

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var morgan = require('morgan');
-var path = require('path');
 var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
